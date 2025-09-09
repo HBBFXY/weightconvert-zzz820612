34400fda9aa77fab2115b6458ae6c90fdc79f765
zzz820612 <zyz198312@qq.com>
2025-09-09 15:47:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# 在这个文件下编写代码，题目具体要求见README.md文件
+#WeightConvert.py
+WeightStr=input("")
+if WeightStr[-2:].lower()=='kg':
+  #输入公斤转化为磅
+  pd=eval(WeightStr[0:-2])*2.2046
+  print("对应的英制重量为{：.3f}磅".format(pd))
+elif WeightStr[-2:].lower()=='pd':
+  #输入为磅转化为公斤
+  kg=eval(WeightStr[0:-2])/2.2046
+  print("对应的公制单位为{:.3f}公斤".format(kg))
+else:
+  print("输入格式错误")
+  
+
+  
+
