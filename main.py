a8ba9d2d81eb10733cc65ab5f8c5e042b89d7663
zzz820612 <zyz198312@qq.com>
2025-09-09 16:23:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ WeightStr=input()
 if WeightStr[-2:].lower()=='kg':
   #输入公斤转化为磅
   pd=eval(WeightStr[0:-2])*2.2046
-  print("对应的英制重量为{：.3f}磅".format(pd))
+  print("对应的英制重量为{:.3f}磅".format(pd))
 elif WeightStr[-2:].lower()=='pd':
   #输入为磅转化为公斤
   kg=eval(WeightStr[0:-2])/2.2046
